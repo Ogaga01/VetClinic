cd9bf38f5e996f4c4cc73558d19f103d5ead68c3
Ogaga <iyaraogaga69@gmail.com>
2022-07-04 17:39:19 +0100

    Add sixth query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ vet_clinic=# SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' A
 vet_clinic=# SELECT name FROM animals WHERE neutered = '1' AND escape_attempts < 3;
 vet_clinic=# SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
 vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+vet_clinic=# SELECT * FROM animals WHERE neutered = '1';
