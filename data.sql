edddc97d919e517aa50a1bec8a81f18d194f5aac
Ogaga <iyaraogaga69@gmail.com>
2022-07-04 16:58:40 +0100

    Modify first animal data

--- data.sql
+++ data.sql
@@ -1,3 +1,3 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, '0', 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, '1', 10.23);
