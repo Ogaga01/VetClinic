d992cae956175eb6998c2c2c92a266c4b6f7254c
Ogaga <iyaraogaga69@gmail.com>
2022-07-04 17:41:12 +0100

    Add seventh query

--- queries.sql
+++ queries.sql
@@ -6,3 +6,4 @@ vet_clinic=# SELECT name FROM animals WHERE neutered = '1' AND escape_attempts <
 vet_clinic=# SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
 vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 vet_clinic=# SELECT * FROM animals WHERE neutered = '1';
+vet_clinic=#  SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
