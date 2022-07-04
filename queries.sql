c7519b45ffeb4cee6e5fcf13f6fa3c0e4a348b11
Ogaga <iyaraogaga69@gmail.com>
2022-07-04 17:30:05 +0100

    Add second query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 vet_clinic=# SELECT * FROM animals WHERE name LIKE '%mon';
+vet_clinic=# SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
