17e6386f1413f3c4c9013c93d37879dae132d210
Ogaga <iyaraogaga69@gmail.com>
2022-07-04 17:33:45 +0100

    Add fourth query

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 vet_clinic=# SELECT * FROM animals WHERE name LIKE '%mon';
 vet_clinic=# SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 vet_clinic=# SELECT name FROM animals WHERE neutered = '1' AND escape_attempts < 3;
+vet_clinic=# SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
