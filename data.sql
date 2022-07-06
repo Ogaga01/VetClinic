46a629302a8539304501da2259f8762519896d64
Ogaga <iyaraogaga69@gmail.com>
2022-07-06 16:13:41 +0100

    Populate owner_id column

--- data.sql
+++ data.sql
@@ -30,3 +30,12 @@ BEGIN;
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
 COMMIT;
+
+BEGIN;
+UPDATE animals SET name = 'Angemon' WHERE name = 'Agemon';
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+COMMIT;
