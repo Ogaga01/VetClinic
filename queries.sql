4adce7950107910aa22f98cc409349e79894fc95
Ogaga <iyaraogaga69@gmail.com>
2022-07-06 14:02:11 +0100

    Made changes requested by reviewer

--- queries.sql
+++ queries.sql
@@ -8,3 +8,38 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = '1';
 SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+COMMIT;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = 'unspecified';
+COMMIT;
+
+BEGIN;
+DROP TABLE animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SAVEPOINT SAVE1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVE1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT COUNT(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT MIN(weight_kg) FROM animals GROUP BY species;
+
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
