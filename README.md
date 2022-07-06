65d88d6ea250084f619d123f204e02a6b618ca85
Ogaga <iyaraogaga69@gmail.com>
2022-07-06 05:18:40 +0100

    made required changes requested by reviewer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# curriculum-databases-projects-template
+# VetClinic
 
 > This template should be used for database related projects at Microverse.
 > Generate your own repository, update this README and edit all files content while working on projects. You should not be adding any new files unless asked otherwise.
@@ -17,15 +17,9 @@ This repository includes files with plain SQL that can be used to recreate a dat
 
 👤 **Author1**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
-
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/Ogaga01)
+- Twitter: [@twitterhandle](https://twitter.com/i_ogaga_n)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/ogaga-iyara)
 
 ## 🤝 Contributing
 
--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-vet_clinic=# SELECT * FROM animals WHERE name LIKE '%mon';
-vet_clinic=# SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-vet_clinic=# SELECT name FROM animals WHERE neutered = '1' AND escape_attempts < 3;
-vet_clinic=# SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
-vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-vet_clinic=# SELECT * FROM animals WHERE neutered = '1';
-vet_clinic=#  SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
-vet_clinic=# SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = '1' AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = '1';
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
