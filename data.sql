25abe9f75387670a86f3f93438c71b1fb9f162d0
Ogaga <iyaraogaga69@gmail.com>
2022-07-06 06:10:11 +0100

    Add species column to animal table

--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, '1', 8);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, '0', 15.04);
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, '1', 11);
+ ALTER TABLE animals ADD species VARCHAR(25);
