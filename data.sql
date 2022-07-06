03991c39e5df8412ad70c46b63fb4a918e86626c
Ogaga <iyaraogaga69@gmail.com>
2022-07-06 06:33:17 +0100

    Add new rows to the animals table

--- data.sql
+++ data.sql
@@ -5,3 +5,11 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, '0', 15.04);
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, '1', 11);
  ALTER TABLE animals ADD species VARCHAR(25);
+ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
+ ('Charmander', '2020-02-08', 0, '0', -11), 
+ ('Plantmon', '2021-11-15', 2, '1', -5.7), 
+ ('Squirtle', '1993-04-02', 3, '0', -12.13), 
+ ('Agemon', '2005-06-12', 1, '1', -45), 
+ ('Boarmon', '2005-06-07', 7, '1', 20.4), 
+ ('Blossom', '1998-10-13', 3, '1', 17), 
+ ('Ditto', '2022-05-14', 4, '1', 22);
